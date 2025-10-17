368d646250d138e56a421ffcc76d8fccbe1a3dfd
labintsev <andrej.labintsev@yandex.ru>
2025-10-17 12:40:42 +0300

    use gemma3:4b

--- chat_pipeline.py
+++ chat_pipeline.py
@@ -52,7 +52,7 @@ text_embedder = SentenceTransformersTextEmbedder(model=embedder_model)
 # Create retriever, chat generator and prompt builder
 retriever = InMemoryEmbeddingRetriever(document_store, top_k=3)
 chat_generator = OllamaChatGenerator(
-    model="owl/t-lite", url="http://127.0.0.1:11434"
+    model="gemma3:4b", url="http://127.0.0.1:11434"
 )
 
 prompt_builder = ChatPromptBuilder(template=template, required_variables=["question"])
